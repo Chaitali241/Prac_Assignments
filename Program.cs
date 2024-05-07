9a313c62bd648d52c9f84264a4eee56c83ba52d6
PracticeAssignment <chaitali.trivedi@tntra.io>
2024-05-07 09:30:29 +0530

    Repository

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Prac_assignments;
+using Prac_assignments.Repository;
+using Prac_assignments.TenantDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
+IMapper mapper = MappingConfiguration.RegisterMaps().CreateMapper();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TenantDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PracticeDB")));
+
+builder.Services.AddDbContext<TenantDBContext>();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddScoped<IUserRepository, UserRepository>(); 
 
 var app = builder.Build();
 
